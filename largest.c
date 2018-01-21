20045e0096950ef980a2c38a5f5c115b84b95b68
athiransk <35651813+athiransk@users.noreply.github.com>
2018-01-21 14:41:30 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,16 @@
+
+ 
+#include<stdio.h>
+ 
+int main()
+{
+ int a, b, c, largest;
+ 
+ printf("Please Enter three different values\n");
+ scanf("%d %d %d", &a, &b, &c);
+ 
+ largest =((a>b && a>c)?a: (b>c)?b:c);
+ 
+ printf("\nLargest number among three is: %d", largest);
+ return 0;
+}
