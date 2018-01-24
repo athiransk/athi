c34980cfc9a2820a54065d5cb29ebcabdb9d61cd
athiransk <35651813+athiransk@users.noreply.github.com>
2018-01-24 16:14:12 +0530

    Update 21.c

--- 21.c
+++ 21.c
@@ -13,20 +13,14 @@ int main()
     {
         scanf("%d", &arr[i]);
     }
-    max = arr[0];
     min = arr[0];
     for(i=1; i<size; i++)
     {
-        if(arr[i] > max)
-        {
-            max = arr[i];
-        }
         if(arr[i] < min)
         {
             min = arr[i];
         }
     }
-    printf("Maximum element = %d\n", max);
     printf("Minimum element = %d", min);
 
     return 0;
