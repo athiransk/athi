ce855e79b6c30ecdc95f0ce0426ccc5bf12feb51
athiransk <35651813+athiransk@users.noreply.github.com>
2018-01-31 19:02:24 +0800

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+    int i,cnt=0;
+    char a[i];
+    printf("enter the string");
+    scanf("%s",&a[i]);
+    for(i=0;a[i]='\0';i++)
+    {
+        if(a[i]=="");
+            cnt++;
+    }
+       printf("%d",cnt); 
+
+}
