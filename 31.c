d18d0ff1c97dab7c6675903419bad5ba4354b0c4
athiransk <35651813+athiransk@users.noreply.github.com>
2018-01-31 18:38:35 +0800

    Create 31.c

---
+++ 31.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+ int main()
+{
+    int i,count=0;
+    char a[i];
+    printf("enter the string");
+    scanf("%s",&a[i]);
+    for(i=0;a[i]!='\0';i++)
+    {
+        count++;
+    }
+    return(count);
+}
