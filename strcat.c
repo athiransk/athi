2490dcb83e170ddd9a558303aa8684c695e210d8
athiransk <35651813+athiransk@users.noreply.github.com>
2018-02-19 16:34:54 +0530

    Update strcat.c

--- strcat.c
+++ strcat.c
@@ -2,27 +2,24 @@
 
 #include<string.h>
 
-int main(void) 
+void main()
 {
-	char str1[50],str2[50];
-	int n1,n2;
+	char a[50],b[50];
+	int x,y;
 	printf("enter the string1:");
-	scanf("%s",str1);
+	scanf("%s",a);
 	printf("enter the string2:");
-	scanf("%s",str2);
-	n1=strlen(str1);
-	n2=strlen(str2);
-	if(n1>n2)
+	scanf("%s",b);
+	x=strlen(a);
+	y=strlen(b);
+	printf("%d",x);
+	printf("%d",y);
+	if(x>y)
 	{
-		printf("\n%s",str1);
+		printf("\n%s",a);
 	}
-	else if(n2>n1)
+	else if(y>x)
 	{
-		printf("\n%s",str2);
+		printf("\n%s",b);
 	}
-	else
-	{
-		printf("\n %s",str2);
-	}
-	return 0;
 }
