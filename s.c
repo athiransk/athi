577e64535a18f2d57063db73b347bb2bbb444494
athiransk <35651813+athiransk@users.noreply.github.com>
2018-02-19 16:51:23 +0530

    Create s.c

---
+++ s.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int n,sum=0,m,i;
+    printf("enter the count");
+    scanf("%d",&m);
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=0;i<=m;i++)
+{
+   sum=sum+i;
+}
+printf("%d",sum);
+}
