b9f412f8e924a6b6a8228219c7afa6b0892cc82c
athiransk <35651813+athiransk@users.noreply.github.com>
2018-02-26 16:06:27 +0530

    Create abc.c

---
+++ abc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+    int i;
+    char s[10];
+    printf("enter the string");
+    gets(s);
+    for(i=0;s[i]!='\0';i++)
+    {
+    if((s[i]>='a') && (s[i]<='z')) 
+    {
+    if(s[i]>='0' && s[i]<='9')
+    {
+    }
+    }
+    }
+    printf("yes");
+
+}
+
+
