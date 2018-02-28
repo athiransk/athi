31cf4552d664cd88cc89431da7e2ede53105187b
athiransk <35651813+athiransk@users.noreply.github.com>
2018-02-28 16:23:55 +0530

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+   int a=10,b=5,c;
+    c=a+b;
+    printf("%d",c);
+    if(c%2==0)
+    {
+        printf("it is even");
+    }
+    else
+    {
+        printf("it is odd");
+    }
+}
