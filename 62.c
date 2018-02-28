76ac9dcdd8e150fbf708e31732ab6f302a848d0b
athiransk <35651813+athiransk@users.noreply.github.com>
2018-02-28 16:12:36 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,18 @@
+*******************************************************************************/
+#include<stdio.h>
+void main()
+{
+    
+    int a[10],i;
+    printf("enter the string");
+    scanf("%d",&a[10]);
+    for(i=0;a[i]!='\0';i++)
+    {
+ if(a[i]==0)
+ {}
+ if(a[i]==1)
+ {}
+    }
+     printf("it is in binary");
+    
+}
