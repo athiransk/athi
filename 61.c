5ef5c498c198c0abe4ef2989c98378a3f220a2ba
athiransk <35651813+athiransk@users.noreply.github.com>
2018-02-28 15:49:24 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    char a[10];
+    int i, k=3;
+    printf("enter the string");
+
+{
+    scanf("%s",&a[i]);
+}
+for(i=0;i<3;i++)
+{
+    printf("%c",a[i]);
+}
+}
+
+
