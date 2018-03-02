09ad5cfd9f61f8608ea49b7dd3779d90487d4ae6
athiransk <35651813+athiransk@users.noreply.github.com>
2018-03-02 18:45:24 +0800

    Create 69.c

---
+++ 69.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int i=10,b=5,c;
+    c=i-b;
+    printf("%d",c);
+    if(c%2==0)
+    {
+        printf("it is an even");
+    }
+    else
+    {
+        printf("it is odd");
+    }
+}
