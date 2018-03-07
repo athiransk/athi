4e00afedc2e6dca7a0cd07db1c788527c480357d
athiransk <35651813+athiransk@users.noreply.github.com>
2018-03-07 19:30:01 +0800

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    printf("enter the number");
+    scanf("%d",i);
+        if(i%13==0)
+        {
+            printf("yes");
+        }
+        else
+        {
+            printf("no");
+        }
+}
