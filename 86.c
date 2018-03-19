6b61b1055345ed01334130dfc2b210f7f7253989
athiransk <35651813+athiransk@users.noreply.github.com>
2018-03-19 18:20:32 +0800

    Create 86.c

---
+++ 86.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+    int i;
+    int a[i],b[i];
+    printf("enter the numbers");
+    for(i=0;i<2;i++)
+    {
+    scanf("%d%d",&a[i],&b[i]);
+    printf("%d\n",b[i]-a[i]);
+}
+}
