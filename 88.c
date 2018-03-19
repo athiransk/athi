39e5c5edd1ee6acc71a82fcde662e7bebe5ca9f2
athiransk <35651813+athiransk@users.noreply.github.com>
2018-03-19 18:49:09 +0800

    Create 88.c

---
+++ 88.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+  int a,b,c,d;
+  printf("enter the number");
+  scanf("%d%d",&a,&b);
+  c=a/b;
+  d=a%b;
+  printf("%d\n",c);
+  printf("%d\n",d);
+}
