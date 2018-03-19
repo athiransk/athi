b121f7d95403ed373ee814c02e96bded21f49a88
athiransk <35651813+athiransk@users.noreply.github.com>
2018-03-19 18:27:52 +0800

    Create 87.c

---
+++ 87.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+  float a,b,c;
+  printf("enter the length");
+  scanf("%f",&a);
+  printf("enter the breadth");
+  scanf("%f",&b);
+  c=a*b;
+  printf("%f",c);
+  return 0;
+}
